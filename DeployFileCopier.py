3e343a4d76c7d8a4d55baea201bd75bf57c7f260
mfaber@amuniversal.com <77071322+kubecode30@users.noreply.github.com>
2021-04-28 13:09:44 -0500

    Started on building out placeholders for deployments

--- DeployFileCopier.py
+++ DeployFileCopier.py
@@ -4,18 +4,21 @@ import shutil
 
 #copies helm files over to a project
 def helmcopy(projectname):
-    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployer-automater\\helm\\"
-    todirectory = "C:\\Users\\mfaber\\Documents\\GitHub\\" + projectname +"\\deployments\\charts\\"
+    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\helm"
+    todirectory = "C:\\Users\\mfaber\\Documents\\GitHub\\" + projectname +"\\deployments\\charts"
     copy_tree(fromdirectory,todirectory)
 
 #copies k8 files over to project
 def k8scopy(projectname):
-    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployer-automater\\k8"
+    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\k8"
     todirectory = "C:\\users\mfaber\\documents\\github\\" +projectname+ "\\deployments\\k8s"
     copy_tree(fromdirectory,todirectory)
 
 #copies workflows to .github directory
 def copygithubworkflows(projectname): 
-    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployer-automater\\.github"
-    todirectory = "C:\\users\\mfaber\\documents\\github\\"+ projectname 
+    fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\.github"
+    todirectory = "C:\\users\\mfaber\\documents\\github\\"+ projectname + "\\.github\\workflows"
     copy_tree(fromdirectory,todirectory)
+
+
+copygithubworkflows("spot-the-difference_game")
--- YamlChanger.py
+++ YamlChanger.py
@@ -8,14 +8,6 @@ def fileopener(path, reponame):
 
         with open(path, 'w') as file: 
             file.write(filedata)
-        
-#this changes the placeholder placeholder with the repo name for the 
-def filechanger(path, reponame): 
-    with fileinput.FileInput as file:
-        for line in file:
-            print(line.replace("placeholder", reponame), end='')
-
-
 
 path = "C:\\Users\\Mfaber\\Documents\\github\\deployment-automater\\.github\\aks-production.yaml"
 reponame = 'content-service'
