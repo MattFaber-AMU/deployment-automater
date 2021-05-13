b7126bc1b048fba4bf22039c2e7cbcd6e04cf63e
mfaber@amuniversal.com <77071322+kubecode30@users.noreply.github.com>
2021-05-13 12:54:19 -0500

    finished filechanger file

--- DeployFileCopier.py
+++ DeployFileCopier.py
@@ -1,24 +1,35 @@
-#this file is to automate creation of files for kubernetes workflows
+# this file is to automate creation of files for kubernetes workflows
 from distutils.dir_util import copy_tree
 import shutil
 
-#copies helm files over to a project
+# copies helm files over to a project
+
+
 def helmcopy(projectname):
     fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\helm"
-    todirectory = "C:\\Users\\mfaber\\Documents\\GitHub\\" + projectname +"\\deployments\\charts"
-    copy_tree(fromdirectory,todirectory)
+    todirectory = "C:\\Users\\mfaber\\Documents\\GitHub\\" + \
+        projectname + "\\deployments\\charts"
+    copy_tree(fromdirectory, todirectory)
 
-#copies k8 files over to project
+# copies k8 files over to project
+
+
 def k8scopy(projectname):
     fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\k8"
-    todirectory = "C:\\users\mfaber\\documents\\github\\" +projectname+ "\\deployments\\k8s"
-    copy_tree(fromdirectory,todirectory)
+    todirectory = "C:\\users\mfaber\\documents\\github\\" + \
+        projectname + "\\deployments\\k8s"
+    copy_tree(fromdirectory, todirectory)
 
-#copies workflows to .github directory
-def copygithubworkflows(projectname): 
+# copies workflows to .github directory
+
+
+def copygithubworkflows(projectname):
     fromdirectory = "C:\\Users\\mfaber\\Documents\\github\\deployment-automater\\.github"
-    todirectory = "C:\\users\\mfaber\\documents\\github\\"+ projectname + "\\.github\\workflows"
-    copy_tree(fromdirectory,todirectory)
+    todirectory = "C:\\users\\mfaber\\documents\\github\\" + \
+        projectname + "\\.github\\workflows"
+    copy_tree(fromdirectory, todirectory)
 
 
-copygithubworkflows("spot-the-difference_game")
+helmcopy("sudoku_game")
+k8scopy("sudoku_game")
+copygithubworkflows("sudoku_game")
--- YamlChanger.py
+++ YamlChanger.py
@@ -1,14 +1,50 @@
+# this changes the elements in the yaml file in order for a successful deploy to happen.
+#!!!!!!!!!!!!!!!!!!IMPORTANT!!!!!!!!!!!!!!!!!!! RUN THIS AFTER DeployFIleCopier.py
+
 import fileinput
 
-def fileopener(path, reponame): 
-    with open(path, "r") as file: 
+# changes the placeholder for the name of the app, with a -
+
+
+def fileopener(frompath, reponame):
+    with open(frompath, "r") as file:
         filedata = file.read()
-        
+
         filedata = filedata.replace('placeholder', reponame)
 
-        with open(path, 'w') as file: 
+        with open(frompath, 'w') as file:
             file.write(filedata)
 
-path = "C:\\Users\\Mfaber\\Documents\\github\\deployment-automater\\.github\\aks-production.yaml"
-reponame = 'content-service'
-fileopener(path, reponame)
+# changes the dockername placeholder to the docker file name
+
+
+def dockernamechange(frompath, reponame):
+    with open(frompath, "r") as file:
+        filedata = file.read()
+        filedata = filedata.replace('dockername', reponame)
+
+        with open(frompath, 'w') as file:
+            file.write(filedata)
+
+
+# change these to the correct names that reflect the app
+reponame = 'content-game'
+dockername = 'content_game'
+
+# change the path to your local github directory
+prodakspath = "C:\\Users\\mfaber\\Documents\\GitHub\\" + \
+    reponame+"\\.github\\workflows\\aks-production.yaml"
+stagepath = "C:\\Users\\mfaber\\Documents\\GitHub\\" + \
+    reponame + "\\.github\\workflows\\aks-staging.yaml"
+valuespath = "C:\\users\\mfaber\\documents\\Github\\" + \
+    reponame + "\\deployments\\Charts\\values.yaml"
+chartpath = "C:\\Users\\mfaber\\documents\\Github\\" + \
+    reponame + "\\deployments\\Charts\Chart.yaml"
+
+# method calls
+fileopener(prodakspath, reponame)
+fileopener(stagepath, reponame)
+fileopener(valuespath, reponame)
+fileopener(chartpath, reponame)
+dockernamechange(stagepath, dockername)
+dockernamechange(prodakspath, dockername)
